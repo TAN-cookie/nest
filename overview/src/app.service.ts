619d38d22e07cfa6091805fe966cc582949468b5
TAN-cookie <mintrayvr@gmail.com>
2021-12-17 01:58:46 +0900

    tutorial controller & service

--- overview/src/app.service.ts
+++ overview/src/app.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Nest!';
+  }
+  getHi(): string {
+    return 'Hi Nest!';
   }
 }
